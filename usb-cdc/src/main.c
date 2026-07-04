87b26a0371243fb95d84bbd6e46fc595c5e7e457
martins-builds <martinsoviawe.dev@gmail.com>
2026-07-04 09:12:52 +0100

    Configured all my registers and wrote the clock init function

--- usb-cdc/src/main.c
+++ usb-cdc/src/main.c
@@ -1,13 +1,42 @@
 #include <stdint.h>
 #include "tusb.h"
 
+#define RCC_BASE    0x40023800
+#define GPIOA_BASE  0x40020000
+#define GPIOB_BASE  0x40020400
+#define OTG_FS      0x50000000
+
 //PLL
 #define RCC_CR       (*(volatile uint32_t*)(RCC_BASE + 0x00))
 #define RCC_PLLCFGR  (*(volatile uint32_t*)(RCC_BASE + 0x04))
 #define RCC_CFGR     (*(volatile uint32_t*)(RCC_BASE + 0x08))
 #define FLASH_ACR    (*(volatile uint32_t*)(0x40023C00 + 0x00))
 
+//PINS
+//port A
+#define RCC_AHB1ENR  (*(volatile uint32_t*)(RCC_BASE   + 0x30))
+#define GPIOA_MODER  (*(volatile uint32_t*)(GPIOA_BASE + 0x00))
+#define GPIOA_ODR    (*(volatile uint32_t*)(GPIOA_BASE + 0x14))
+#define GPIOA_AFRL   (*(volatile uint32_t*)(GPIOA_BASE + 0x20))
+#define GPIOA_OTYPER (*(volatile uint32_t*)(GPIOA_BASE + 0x04))
+
+//port B
+#define GPIOB_MODER   (*(volatile uint32_t*)(GPIOB_BASE + 0x00))
+#define GPIOB_OTYPER  (*(volatile uint32_t*)(GPIOB_BASE + 0x04))
+#define GPIOB_OSPEEDR (*(volatile uint32_t*)(GPIOB_BASE + 0x08))
+#define GPIOB_AFRH    (*(volatile uint32_t*)(GPIOB_BASE + 0x24))
+
 //OTG
+#define RCC_AHB2ENR   (*(volatile uint32_t*)(RCC_BASE  + 0x34))
+
+#define OTG_GAHBCFG   (*(volatile uint32_t*)(OTG_FS  + 0x008))
+#define OTG_GINTMSK   (*(volatile uint32_t*)(OTG_FS  + 0x018))
+#define OTG_GINTSTS   (*(volatile uint32_t*)(OTG_FS  + 0x014))
+#define OTG_GUSBCFG   (*(volatile uint32_t*)(OTG_FS  + 0x00C))
+#define OTG_DCFG      (*(volatile uint32_t*)(OTG_FS  + 0x800))
+#define OTG_DTHRCTL   (*(volatile uint32_t*)(OTG_FS  + 0x0830))
+#define OTG_DCTL      (*(volatile uint32_t*)(OTG_FS  + 0x804))
+#define OTG_DSTS      (*(volatile uint32_t*)(OTG_FS  + 0x808))
 
 void clock_init(){
     FLASH_ACR |= (5 << 0);
